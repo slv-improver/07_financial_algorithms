a11e5e7a157561c101be4e62ca89e5d92f7aebc4
slv-improver <k.moret.slv@gmail.com>
2022-08-25 15:05:41 +0100

    change benefits to profits

--- bruteforce.py
+++ bruteforce.py
@@ -4,33 +4,33 @@ import time
 start = time.process_time()
 
 SHARES = [
-    {'name': 'Action-1', 'price': 20, 'benefits': 5},
-    {'name': 'Action-2', 'price': 30, 'benefits': 10},
-    {'name': 'Action-3', 'price': 50, 'benefits': 15},
-    {'name': 'Action-4', 'price': 70, 'benefits': 20},
-    {'name': 'Action-5', 'price': 60, 'benefits': 17},
-    {'name': 'Action-6', 'price': 80, 'benefits': 25},
-    {'name': 'Action-7', 'price': 22, 'benefits': 7},
-    {'name': 'Action-8', 'price': 26, 'benefits': 11},
-    {'name': 'Action-9', 'price': 48, 'benefits': 13},
-    {'name': 'Action-10', 'price': 34, 'benefits': 27},
-    {'name': 'Action-11', 'price': 42, 'benefits': 17},
-    {'name': 'Action-12', 'price': 110, 'benefits': 9},
-    {'name': 'Action-13', 'price': 38, 'benefits': 23},
-    {'name': 'Action-14', 'price': 14, 'benefits': 1},
-    {'name': 'Action-15', 'price': 18, 'benefits': 3},
-    {'name': 'Action-16', 'price': 8, 'benefits': 8},
-    {'name': 'Action-17', 'price': 4, 'benefits': 12},
-    {'name': 'Action-18', 'price': 10, 'benefits': 14},
-    {'name': 'Action-19', 'price': 24, 'benefits': 21},
-    {'name': 'Action-20', 'price': 114, 'benefits': 18},
+    {'name': 'Action-1', 'price': 20, 'profits': 5},
+    {'name': 'Action-2', 'price': 30, 'profits': 10},
+    {'name': 'Action-3', 'price': 50, 'profits': 15},
+    {'name': 'Action-4', 'price': 70, 'profits': 20},
+    {'name': 'Action-5', 'price': 60, 'profits': 17},
+    {'name': 'Action-6', 'price': 80, 'profits': 25},
+    {'name': 'Action-7', 'price': 22, 'profits': 7},
+    {'name': 'Action-8', 'price': 26, 'profits': 11},
+    {'name': 'Action-9', 'price': 48, 'profits': 13},
+    {'name': 'Action-10', 'price': 34, 'profits': 27},
+    {'name': 'Action-11', 'price': 42, 'profits': 17},
+    {'name': 'Action-12', 'price': 110, 'profits': 9},
+    {'name': 'Action-13', 'price': 38, 'profits': 23},
+    {'name': 'Action-14', 'price': 14, 'profits': 1},
+    {'name': 'Action-15', 'price': 18, 'profits': 3},
+    {'name': 'Action-16', 'price': 8, 'profits': 8},
+    {'name': 'Action-17', 'price': 4, 'profits': 12},
+    {'name': 'Action-18', 'price': 10, 'profits': 14},
+    {'name': 'Action-19', 'price': 24, 'profits': 21},
+    {'name': 'Action-20', 'price': 114, 'profits': 18},
 ]
 CUSTOMER_BUDGET = 500
 
 combinations_list = []
 
 
-def benefits_calc(price, percent):
+def profits_calc(price, percent):
     return price*percent/100
 
 
@@ -56,7 +56,7 @@ def binary_tree_investment(index: int, remaining_budget: int, combination: list)
 
 def bruteforce():
     """ Try all investment combinations.
-    And find the one with most benefits.
+    And find the one with most profits.
     """
     binary_tree_investment(0, CUSTOMER_BUDGET, [])
     print(time.process_time() - start, 's')
@@ -65,9 +65,9 @@ def bruteforce():
     for i, combination in enumerate(combinations_list):
         invest_combination = {'index': i, 'total': 0}
         for share in combination:
-            invest_combination['total'] += benefits_calc(
+            invest_combination['total'] += profits_calc(
                 share['price'],
-                share['benefits'],
+                share['profits'],
             )
         if invest_combination['total'] > best_combination['total']:
             best_combination = invest_combination
