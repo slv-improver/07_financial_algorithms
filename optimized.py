4d0b9abe21d5f105c715dc0f385904b1cd338f2c
slv-improver <k.moret.slv@gmail.com>
2022-08-27 20:36:48 +0100

    create shares.csv & read it in optmized_investment()

--- optimized.py
+++ optimized.py
@@ -1,11 +1,18 @@
 import time
+import csv
 
 
 start = time.process_time()
 
 
 def optimized_investment():
-    pass
+    with open('shares.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            print(f'\t{row["name"]}, {row["price"]}, {row["profit"]}')
+            line_count += 1
+        print(f'\nProcessed {line_count} lines in:')
 
 
 if __name__ == '__main__':
