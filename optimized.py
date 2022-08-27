db2be355b4e5643451c76369078eb0aecacc3e0c
slv-improver <k.moret.slv@gmail.com>
2022-08-27 20:26:11 +0100

    create optimzed.py to optimize bruteforce.py

---
+++ optimized.py
@@ -0,0 +1,13 @@
+import time
+
+
+start = time.process_time()
+
+
+def optimized_investment():
+    pass
+
+
+if __name__ == '__main__':
+    optimized_investment()
+    print(time.process_time() - start, 's')
