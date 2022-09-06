72549cc5c36b5a60b4de02c54b8de0175fc91372
slv-improver <k.moret.slv@gmail.com>
2022-09-06 17:24:10 +0100

    rename 'matrice' variable to 'matrix'

--- optimized.py
+++ optimized.py
@@ -42,18 +42,18 @@ def dynamic_algo(budget, shares_list):
     """
     # Convert budget to cents to get integer price
     budget *= 100
-    matrice = [[0 for x in range(budget + 1)] for x in range(len(shares_list) + 1)]
+    matrix = [[0 for x in range(budget + 1)] for x in range(len(shares_list) + 1)]
 
     for i in range(1, len(shares_list) + 1):
         for b in range(1, budget + 1):
             if shares_list[i-1]['price'] <= b:
-                matrice[i][b] = max(
+                matrix[i][b] = max(
                     shares_list[i-1]['profit_amount']
-                    + matrice[i-1][b-shares_list[i-1]['price']],
-                    matrice[i-1][b],
+                    + matrix[i-1][b-shares_list[i-1]['price']],
+                    matrix[i-1][b],
                 )
             else:
-                matrice[i][b] = matrice[i - 1][b]
+                matrix[i][b] = matrix[i - 1][b]
 
     b = budget
     n = len(shares_list)
@@ -61,13 +61,13 @@ def dynamic_algo(budget, shares_list):
 
     while b >= 0 and n >= 0:
         share = shares_list[n - 1]
-        if matrice[n][b] == matrice[n - 1][b - share['price']] + share['profit_amount']:
+        if matrix[n][b] == matrix[n - 1][b - share['price']] + share['profit_amount']:
             shares_selection.append(share)
             b -= share['price']
 
         n -= 1
 
-    print(shares_selection, (budget - b) / 100, matrice[-1][-1], sep='\n- ')
+    print(shares_selection, (budget - b) / 100, matrix[-1][-1], sep='\n- ')
 
 
 def optimized_investment():
